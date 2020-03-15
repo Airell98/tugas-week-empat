52574b0ad48638369ec76bf1026b9240d67030e4
Airell98 <57603433+Airell98@users.noreply.github.com>
2020-03-15 21:53:39 +0700

    Add files via upload

---
+++ ubah-kata.js
@@ -0,0 +1,24 @@
+function ubahHuruf(kata) {
+    // you can only write your code here!
+    var abjad = 'abcdefghijklmnopqrstuvwxyz';
+    var str = ''
+     for(var i = 0; i < kata.length; i++){
+         for(var j =0; j < abjad.length; j++){
+            if(kata[i] === abjad[j]){
+                str += abjad[j+1]
+            }
+            if(kata[i] === 'z'){
+                str += 'a'
+                break
+            }
+         }
+     }
+     return str;
+  }
+  
+  // TEST CASES
+  console.log(ubahHuruf('wow')); // xpx
+   console.log(ubahHuruf('developer')); // efwfmpqfs
+   console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+   console.log(ubahHuruf('keren')); // lfsfo
+   console.log(ubahHuruf('semangat')); // tfnbohbu
