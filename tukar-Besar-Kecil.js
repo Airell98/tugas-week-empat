b7609dc9f984da060b4158cf572de745e1bc90de
Airell98 <57603433+Airell98@users.noreply.github.com>
2020-03-15 22:06:29 +0700

    Add files via upload

---
+++ tukar-Besar-Kecil.js
@@ -0,0 +1,30 @@
+function tukarBesarKecil(kalimat) {
+	// you can only write your code here!
+	var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; //Hello World
+	var lower = 'abcdefghijklmnopqrstuvwxyz';
+	var tampung = '';
+
+	for (var i = 0; i < kalimat.length; i++) {
+		var check = true;
+		for (var j = 0; j < lower.length; j++) {
+			if (kalimat[i] === lower[j]) {
+                tampung += upper[j];
+                 check = false;
+			} else if (kalimat[i] === upper[j]) {
+                tampung += lower[j];
+                 check = false;
+			}
+        }
+        if(check){
+            tampung += kalimat[i]
+        }
+	}
+	return tampung;
+}
+
+// TEST CASES
+console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
